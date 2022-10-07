9ded9f854c3e8900f6d7e0ea6a0435c8cbcc6d01
sampath <sampathr@hsenidmobile.com>
2022-10-07 22:17:51 +0530

    Add sample using jackson

---
+++ src/main/java/com/hms/App.java
@@ -0,0 +1,13 @@
+package com.hms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
